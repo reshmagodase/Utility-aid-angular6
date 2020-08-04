3b2cdfdea79e2c10942a257f0ab8585a0008e713
Dnyaneshwar <dnyaneshwar@scriptlanes.com>
2020-08-04 13:16:21 +0530

    meta info changed

--- src/app/thankyou/thankyou.component.ts
+++ src/app/thankyou/thankyou.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MetaserviceService } from '../metaservice.service';
 
 @Component({
   selector: 'app-thankyou',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThankyouComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: MetaserviceService) { }
 
   ngOnInit(): void {
+    this.meta.updateMetaInfo(
+      "We're inspired by the organisations and people we work with. We want to help save them time and money when they source and purchase their energy.",
+      "Thank You",
+      "assets/contact.jpg",
+      "thankyou"
+    );
+    this.meta.updateTitle("", "Thank You");
   }
 
 }
